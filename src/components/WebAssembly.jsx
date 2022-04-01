d860200c03591a602b9400725f274ac43a3cca16
jeff1010322 <jeff1010322@gmail.com>
2022-03-31 23:51:09 -0400

    fix: use base url to load wasm to correctly build for dev and prod

--- src/components/WebAssembly.jsx
+++ src/components/WebAssembly.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import useBaseUrl from '@docusaurus/useBaseUrl';
 
 export const WebAssembly = () => {
 
   return (
     <iframe 
-      src="../wasm.html"
+      src={useBaseUrl("wasm")}
       width="420" 
       height="600"
       allow="autoplay"></iframe>
   )
-  }
+}
